bf33a01d1ae206c86ff3fe2ad1dafdd1642be622
althomi3 <alischa.thomas@aalto.fi>
2025-05-10 10:40:37 +0200

    adds world, camera, sets game dimensions

---
+++ lib/src/components/play_area.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import '../brick_breaker.dart';
+
+
+// Defines Play Area
+class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
+  PlayArea()
+      : super(
+          paint: Paint()..color = const Color(0xfff2e8cf),
+        );
+
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(game.width, game.height);
+  }
+}
+
